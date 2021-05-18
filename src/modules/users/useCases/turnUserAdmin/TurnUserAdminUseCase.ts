6a66a8644639b29b59c674eb7e0a7aea5b33d7b6
Matheus Alves <matheusalves789@gmail.com>
2021-05-18 11:12:41 -0300

    Test 100%

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    let user = this.usersRepository.findById(user_id);
+
+    if (user) {
+      user = this.usersRepository.turnAdmin(user);
+    } else {
+      throw new Error("User not found");
+    }
+
+    return user;
   }
 }
 
